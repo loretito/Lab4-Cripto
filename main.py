d82d2beeb5e44edce99231c0fd9632715bf5560a
Loreto <loreto.nancucheo@gmail.com>
2024-11-14 15:21:55 -0300

    refactor: :recycle: Improve output formatting in encryption and decryption process

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def process_data(option, data):
     if option == "s":
         key, iv = input_key_iv("Todos los algoritmos")
         
-        print("\n\033[35m === CIFRADO ===\033[0m")
+        print("\n\033[35m\n === CIFRADO ===\033[0m\n")
         print("\n\033[34mTexto original:\033[0m", data)
         
         for algorithm, key_size, iv_size in algorithms:
@@ -20,7 +20,7 @@ def process_data(option, data):
             encrypted_data.append((encrypted, key_out, iv_out, algorithm, key_size, iv_size))
 
     else:
-        print("\n\033[35m === CIFRADO ===\033[0m")
+        print("\n\033[35m\n === CIFRADO ===\033[0m\n")
         print(f"\n\033[34mTexto original:\033[0m {data}")
         
         for algorithm, key_size, iv_size in algorithms:
@@ -28,19 +28,19 @@ def process_data(option, data):
             encrypted, key_out, iv_out = encrypt_and_print(data, key, iv, algorithm, key_size, iv_size)
             encrypted_data.append((encrypted, key_out, iv_out, algorithm, key_size, iv_size))
 
-    print("\n\033[35m === DESCIFRADO ===\033[0m")
+    print("\n\033[35m\n === DESCIFRADO ===\033[0m\n")
     for encrypted, key_out, iv_out, algorithm, key_size, iv_size in encrypted_data:
         decrypt_and_print(encrypted, key_out, iv_out, algorithm, key_size, iv_size)
 
 def main():
-    print("\n\033[35m== CIFRADO Y DESCIFRADO ==\033[0m\n\n")
+    print("\n\033[35m=== CIFRADO Y DESCIFRADO (AES/DES/3DES) ===\033[0m\n\n")
 
     print("Utilizar la misma llave/IV para todos los algoritmos? (s/n)")
     while True:
         option = input("s/n: ").lower()
         if option in ["s", "n"]:
             break
-        print("Opción no válida.")
+        print("❌ Opción no válida.\n")
 
     data = input("\nTexto a encriptar: ").encode()
     process_data(option, data)
